4db66ab27485b834a525024b9b16ea0d530edc6f
Pieplu <alexis.pieplu@gmail.com>
2016-09-08 08:16:41 -0400

    Get calendars url from propfind

--- index.php
+++ index.php
@@ -12,35 +12,28 @@ $username = $configs['username'];
 $password = $configs['password'];
 
 $calendarsArray = array("clbrations","tudes");
+$calendarsArrayFromServer = array();
+$calendarsArrayOfUrl = array();
 $arrayOfics = array();
-// Get ownCloud calendar
 
-$xml = '<?xml version="1.0" encoding="UTF-8"?>
-<D:propfind xmlns:D="DAV:" xmlns:CS="http://calendarserver.org/ns/" xmlns:C="urn:ietf:params:xml:ns:caldav">
-    <D:prop>
-        <D:displayname />
-        <C:calendar-description />
-        <D:owner />
-        <D:current-user-principal />
-    </D:prop>
-</D:propfind>';
-//
-// $curlDav = curl_init($remoteHost . '/remote.php/caldav/calendars/'.$username.'/');
-// curl_setopt($curlDav, CURLOPT_USERPWD, $username . ":" . $password);
-// curl_setopt($curlDav, CURLOPT_CUSTOMREQUEST, 'PROPFIND');
-// curl_setopt($curlDav, CURLOPT_POSTFIELDS, $xml);
-// //CURLOPT_HTTPHEADER, array('Content-Type: multipart/form-data;
-// curl_setopt($curlDav, CURLOPT_RETURNTRANSFER, TRUE);
-// curl_setopt($curlDav, CURLOPT_VERBOSE, true);
-// $DAVCal = curl_exec($curlDav);
-// curl_close($curlDav);
-//
-// echo $DAVCal;
+// Get ownCloud calendars
+$curlDav = curl_init($remoteHost . '/remote.php/caldav/calendars/'.$username.'/');
+curl_setopt($curlDav, CURLOPT_USERPWD, $username . ":" . $password);
+curl_setopt($curlDav, CURLOPT_CUSTOMREQUEST, 'PROPFIND');
+curl_setopt($curlDav, CURLOPT_RETURNTRANSFER, TRUE);
+$DAVCal = curl_exec($curlDav);
+curl_close($curlDav);
+
+preg_match_all("/(\/remote\.php\/caldav\/calendars\/\w+\/\w+\/)/", $DAVCal, $calendarsArrayFromServer);
+
+$calendarsArrayOfUrl = $calendarsArrayFromServer[0];
+
+// TODO filter /remote.php/caldav/calendars/admin/inbox/ and /remote.php/caldav/calendars/admin/outbox/
 
 
-foreach($calendarsArray as $value){
+foreach($calendarsArrayOfUrl as $value){
   echo $value."<br>";
-  $curl = curl_init($remoteHost . '/remote.php/caldav/calendars/'.$username.'/'.$value.'?export');
+  $curl = curl_init($remoteHost . $value . '?export');
   curl_setopt($curl, CURLOPT_USERPWD, $username . ":" . $password);
   curl_setopt($curl, CURLOPT_RETURNTRANSFER, TRUE);
   curl_setopt($curl, CURLOPT_VERBOSE, true);
