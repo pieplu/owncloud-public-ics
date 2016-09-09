97eaecd3ddade385fd63c2fa1539315006962660
Alexis Piéplu <alexis.pieplu@savoirfairelinux.com>
2016-09-09 14:10:26 -0400

    Functional generation of ics

--- index.php
+++ index.php
@@ -28,16 +28,16 @@ preg_match_all("/(\/remote\.php\/caldav\/calendars\/\w+\/\w+\/)/", $DAVCal, $cal
 
 $calendarsArrayOfUrl = $calendarsArrayFromServer[0];
 
-// TODO filter /remote.php/caldav/calendars/admin/inbox/ and /remote.php/caldav/calendars/admin/outbox/
-
 
 foreach($calendarsArrayOfUrl as $value){
-  echo $value."<br>";
   $curl = curl_init($remoteHost . $value . '?export');
   curl_setopt($curl, CURLOPT_USERPWD, $username . ":" . $password);
   curl_setopt($curl, CURLOPT_RETURNTRANSFER, TRUE);
   curl_setopt($curl, CURLOPT_VERBOSE, true);
-  $arrayOfics[$value] = curl_exec($curl);
+  $result = curl_exec($curl);
+  if (curl_getinfo($curl, CURLINFO_HTTP_CODE) == 200){
+    $arrayOfics[explode("/", $value)[5]] = $result;
+  }
   curl_close($curl);
 }
 
@@ -45,30 +45,25 @@ foreach($calendarsArrayOfUrl as $value){
 $globalVCalendar = new VObject\Component\VCalendar();
 
 foreach($arrayOfics as $key => $value){
-    //echo $value."<br><br>";
+    echo $key."<br><br>";
     try {
       $anCalendar = VObject\Reader::read($value);
-    } catch{
-
-    }
 
       if((boolean)$anCalendar->VEVENT){
         foreach($anCalendar->VEVENT as $event) {
-         echo (string)$event->SUMMARY."<br>";
           $globalVCalendar->add($event);
         }
-        file_put_contents('./'.$key.'.ics', $anCalendar->serialize());
       }
+
+      file_put_contents('./'.$key.'.ics', $anCalendar->serialize());
+
+    } catch (ParseException $e){
+      //echo $e;
+    }
+
 }
 
 file_put_contents('./all.ics', $globalVCalendar->serialize());
 
-
-
-// Parse calendar file
-// $calendar = VObject\Reader::read($ics);
-
-// Put the resulting ICS to .public.ics
-// file_put_contents('./public.ics', $calendar->serialize());
 
 ?>
